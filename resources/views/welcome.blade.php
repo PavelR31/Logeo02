c917fe83f258e9daa7d4444aa2cbfe17d4299a7c
TuxBoys <nirobots23@gmail.com>
2024-07-15 21:34:09 -0600

    Primer commit: Subiendo mi proyecto a GitHub

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                   <span>
+                        Primer proyecto POW 2, sistema de logeo!!
+                   </span>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
